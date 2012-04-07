84cfaa8e81baee7209f45d93800f096b974e7240
SebastianDotNet <SebastianDotNet@codeplex.com>
2012-04-07 06:26:27 +0000

    - delete outlook examples
    Imported from NetOffice r88930
    https://netoffice.codeplex.com/SourceControl/changeset/88930

--- Word/C#AExamples/WordExamples/Examples/Example02.cs
+++ Word/C#AExamples/WordExamples/Examples/Example02.cs
@@ -12,7 +12,7 @@ using NetOffice.WordApi.Enums;
 
 namespace WordExamples
 {
-    class Example01 : IExample
+    class Example02 : IExample
     {
         IHost _hostApplication;
 
@@ -30,16 +30,30 @@ namespace WordExamples
             // add a new document
             Word.Document newDocument = wordApplication.Documents.Add();
 
-            // insert some text
-            wordApplication.Selection.TypeText("This text is written by NetOffice");
+            // add a table
+            Word.Table table = newDocument.Tables.Add(wordApplication.Selection.Range, 3, 2);
 
-            wordApplication.Selection.HomeKey(WdUnits.wdLine, WdMovementType.wdExtend);
-            wordApplication.Selection.Font.Color = WdColor.wdColorSeaGreen;
-            wordApplication.Selection.Font.Bold = 1;
-            wordApplication.Selection.Font.Size = 18;
+            // insert some text into the cells
+            table.Cell(1, 1).Select();
+            wordApplication.Selection.TypeText("This");
+
+            table.Cell(1, 2).Select();
+            wordApplication.Selection.TypeText("table");
+
+            table.Cell(2, 1).Select();
+            wordApplication.Selection.TypeText("was");
+
+            table.Cell(2, 2).Select();
+            wordApplication.Selection.TypeText("created");
+
+            table.Cell(3, 1).Select();
+            wordApplication.Selection.TypeText("by");
+
+            table.Cell(3, 2).Select();
+            wordApplication.Selection.TypeText("NetOffice");
 
             // we save the document as .doc for compatibility with all word versions
-            string documentFile = string.Format("{0}\\Example01{1}", _hostApplication.RootDirectory, ".doc");
+            string documentFile = string.Format("{0}\\Example02{1}", _hostApplication.RootDirectory, ".doc");
             newDocument.SaveAs(documentFile, WdSaveFormat.wdFormatDocumentDefault);
 
             // close word and dispose reference
@@ -57,12 +71,12 @@ namespace WordExamples
 
         public string Caption
         {
-            get { return _hostApplication.LCID == 1033 ? "Example01" : "Beispiel01"; }
+            get { return _hostApplication.LCID == 1033 ? "Example02" : "Beispiel02"; }
         }
 
         public string Description
         {
-            get { return _hostApplication.LCID == 1033 ? "Create a document write text and save" : "Dokument erstellen, Text schreiben und speichern"; }
+            get { return _hostApplication.LCID == 1033 ? "Insert a table to document" : "Eine Tabelle erstellen"; }
         }
 
         public UserControl Panel
