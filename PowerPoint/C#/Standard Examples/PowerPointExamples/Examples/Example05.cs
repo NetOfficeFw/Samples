4501b943fa4732fdd70c834df6fa2b233ad14b16
Jozef Izso <jozef.izso@gmail.com>
2017-04-08 12:38:34 +0200

    Update PowerPoint automation samples with NetOffice.PowerPoint nuget package

--- PowerPoint/C#/Standard Examples/PowerPointExamples/Examples/Example05.cs
+++ PowerPoint/C#/Standard Examples/PowerPointExamples/Examples/Example05.cs
@@ -8,7 +8,7 @@ using NetOffice;
 using PowerPoint = NetOffice.PowerPointApi;
 using NetOffice.PowerPointApi.Enums;
 using NetOffice.OfficeApi.Enums;
-using NetOffice.PowerPointApi.Tools.Utils;
+using NetOffice.PowerPointApi.Tools;
 
 namespace PowerPointExamplesCS4
 {
