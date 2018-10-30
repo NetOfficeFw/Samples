478b6c7dab82d6f552486c0ab48190eec6cb5de1
Jozef Izso <jozef.izso@gmail.com>
2018-10-30 14:36:50 +0100

    Update Access samples for NetOffice 1.7.3.0

--- Access/01 Access Automation Sample/AccessExamples/Examples/Example05.cs
+++ Access/01 Access Automation Sample/AccessExamples/Examples/Example05.cs
@@ -9,7 +9,7 @@ using NetOffice.OfficeApi.Enums;
 using DAO = NetOffice.DAOApi;
 using NetOffice.DAOApi.Enums;
 using NetOffice.DAOApi.Constants;
-using NetOffice.AccessApi.Tools.Contribution;
+using NetOffice.AccessApi.Tools;
 
 namespace AccessExamplesCS4
 {
