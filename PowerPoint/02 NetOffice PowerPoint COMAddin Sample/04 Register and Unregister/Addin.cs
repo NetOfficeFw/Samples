0c826c8b5409444f9e3cb4787f75672876ba11e7
Jozef Izso <jozef.izso@gmail.com>
2019-03-03 18:43:27 +0100

    Add missing PowerPoint samples for NetOffice 1.7.4.5

--- PowerPoint/02 NetOffice PowerPoint COMAddin Sample/04 Register and Unregister/Addin.cs
+++ PowerPoint/02 NetOffice PowerPoint COMAddin Sample/04 Register and Unregister/Addin.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Runtime.InteropServices;
-using NetOffice;
 using NetOffice.Tools;
 using Office = NetOffice.OfficeApi;
-using NetOffice.OfficeApi.Enums;
 using PowerPoint = NetOffice.PowerPointApi;
-using NetOffice.PowerPointApi.Enums;
 using NetOffice.PowerPointApi.Tools;
 /*
    Register Addin Example
@@ -17,13 +13,13 @@ namespace PowerPoint04AddinCS4
     [ProgId("PowerPoint04AddinCS4.Connect"), Guid("A5FD2DA0-8F65-4AB1-862A-B9D01D65ECCB"), Codebase, Timestamp]
     [RegistryLocation(RegistrySaveLocation.InstallScopeCurrentUser)]
     public class Addin : COMAddin
-    {       
+    {
         [RegisterFunction(RegisterMode.CallAfter)]  // We want that NetOffice call this method after register
         private static void Register(Type type, RegisterCall registerCall, InstallScope scope, OfficeRegisterKeyState keyState)
         {
 
         }
-        
+
         [UnRegisterFunction(RegisterMode.CallBeforeAndAfter)] // We want that NetOffice call this method before and after unregister
         private static void UnRegister(Type type, RegisterCall registerCall, InstallScope scope, OfficeUnRegisterKeyState keyState)
         {
